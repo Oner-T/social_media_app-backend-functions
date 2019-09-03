96f0346e833ceb10f16103a1a18189c43478d38a
Öner Taylan Öztürk <onertaylanozturk@gmail.com>
2019-09-03 13:35:13 +0300

    continue

--- functions/index.js
+++ functions/index.js
@@ -1,117 +1,28 @@
 const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+
 const express = require("express");
-const serviceAccount = require("./serviceAccountKey.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://social-app-c2671.firebaseio.com"
-});
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCGl_IRAplrD_RBfecGoavKpDnx5WBDrio",
-  authDomain: "social-app-c2671.firebaseapp.com",
-  databaseURL: "https://social-app-c2671.firebaseio.com",
-  projectId: "social-app-c2671",
-  storageBucket: "social-app-c2671.appspot.com",
-  messagingSenderId: "961039627195",
-  appId: "1:961039627195:web:c22ea38a52d0becd"
-};
+const { getAllScreams, postOneScream } = require("./handlers/screams");
+const {signup, login} = require(".handlers/users");
 
 const app = express();
 
 const firebase = require("firebase");
 firebase.initializeApp(firebaseConfig);
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 
-const db = admin.firestore();
 
-app.get("/screams", (req, res) => {
-  db.collection("screams")
-    .orderBy("createdAt", "desc")
-    .get()
-    .then(data => {
-      let screams = [];
-      data.forEach(doc => {
-        screams.push({
-          screamId: doc.id,
-          body: doc.data().body,
-          userHandle: doc.data().userHandle,
-          createdAt: doc.data().createdAt
-        });
-      });
-      return res.json(screams);
-    })
-    .catch(err => {
-      console.error(err);
-    });
-});
+//screams routes
+app.get("/screams", getAllScreams);
+app.post("/scream", FBAuth, postOneScream);
 
-app.post("/scream", (req, res) => {
-  const newScream = {
-    body: req.body.body,
-    userHandle: req.body.userHandle,
-    createdAt: new Date().toISOString()
-  };
+//users routes
+app.post("/signup", signup);
+app.post("/login", login);
 
-  db.collection("screams")
-    .add(newScream)
-    .then(doc => {
-      res.json({ message: `document ${doc.id} created successfully` });
-    })
-    .catch(err => {
-      res.status(500).json({ error: "something went wrong!" });
-      console.error(err);
-    });
-});
 
-app.post("/signup", (req, res) => {
-  const newUser = {
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-    handle: req.body.handle
-  };
 
-  let token, userId;
-  db.doc(`/users/${newUser.handle}`)
-    .get()
-    .then(doc => {
-      if (doc.exists) {
-        return res.status(400).json({ handle: "this handle is already taken" });
-      } else {
-        return firebase
-          .auth()
-          .createUserWithEmailAndPassword(newUser.email, newUser.password);
-      }
-    })
-    .then(data => {
-      userId = data.user.uid;
-      return data.user.getIdToken();
-    })
-    .then(idToken => {
-      token = idToken;
-      const userCredentials = {
-        handle: newUser.handle,
-        email: newUser.email,
-        createdAt: new Date().toISOString(),
-        userId
-      };
-      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
-    })
-    .then(() => {
-      return res.status(201).json({ token });
-    })
-    .catch(err => {
-      console.error(err);
-      if (err.code === "auth/email-already-in-use") {
-        return res.status(400).json({ email: "Email is already in use" });
-      } else {
-        return res.status(500).json({ error: err.code });
-      }
-    });
-});
+
+
 
 exports.api = functions.region("europe-west2").https.onRequest(app);
---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyCGl_IRAplrD_RBfecGoavKpDnx5WBDrio",
+    authDomain: "social-app-c2671.firebaseapp.com",
+    databaseURL: "https://social-app-c2671.firebaseio.com",
+    projectId: "social-app-c2671",
+    storageBucket: "social-app-c2671.appspot.com",
+    messagingSenderId: "961039627195",
+    appId: "1:961039627195:web:c22ea38a52d0becd"
+  };
